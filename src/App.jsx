db770a1d2eb62ab0ba023b08163544905faa07de
Renan Veronez Drechsler <renanvz@outlook.com>
2022-07-13 09:17:21 +0100

    Iterando no jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,38 @@ import { Header } from './components/Header'
 import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar'
 
-
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/renanvzd.png",
+      name: "Renan Veronez",
+      role: "Front-End Developer",
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera', },
+      { type: 'paragraph', content: 'Acabei de subir um post', },
+      { type: 'link', content: 'teste.design/siteprototipo', },
+      { type: '', content: '', },
+    ],
+    publishedAt: new Date('2022-07-5 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/renanvzd.png",
+      name: "Renan Drechsler",
+      role: "Back-End Developer",
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera', },
+      { type: 'paragraph', content: 'Acabei de subir um post', },
+      { type: 'link', content: 'teste.design/siteprototipo', },
+      { type: '', content: '', },
+    ],
+    publishedAt: new Date('2022-07-11 20:00:00'),
+  },
+]
 
 export function App() {
   return (
@@ -15,14 +46,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Renan Veronez"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit"
-          />
-          <Post
-            author="Author II"
-            content="New Post"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
 
